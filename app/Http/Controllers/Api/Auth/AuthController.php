d2801b538442b02ef0bf1a374818e64568d5d6c4
Danillo Ferreira <nilloferreiira@gmail.com>
2025-11-07 01:31:27 -0300

    feat: jwt auth

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,16 +5,32 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\SignInRequest;
 use App\Http\Requests\Auth\SignUpRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
     public function signIn(SignInRequest $request)
     {
-        // Authentication logic here
+        $credentials = $request->only(['email', 'password']);
+
+        $accessToken = JWTAuth::attempt($credentials);
+
+        if (!$accessToken) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+        $user = Auth::user();
+        return response()->json(['access_token' => $accessToken, 'user' => $user], 200);
     }
 
     public function signUp(SignUpRequest $request)
     {
-        return response()->json(['message' => 'User registered successfully']);
+        $user = User::create($request->validated());
+        if ($user) {
+            return response()->json(['message' => 'User registered successfully'], 201);
+        }
+
+        return response()->json(['message' => 'User registration failed'], 500);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
@@ -42,4 +43,35 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+    // Rest omitted for brevity
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+            'name' => $this->name,
+            'user_id' => $this->id
+            //role
+        ];
+    }
 }
