eee9d5ab85cd41dd5998041134461314828bf034
Danillo Ferreira <nilloferreiira@gmail.com>
2025-11-19 16:43:13 -0300

    fix: agora vai

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # filepath: /home/danillo/www/valet/flap-api/entrypoint.sh
 
 # Aguarda o MySQL ficar disponível
-until mysql -h db -u root -p"$MYSQL_ROOT_PASSWORD" -e "SHOW DATABASES;" > /dev/null 2>&1; do
+until mysql -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "SHOW DATABASES;" > /dev/null 2>&1; do
   echo "Aguardando o banco de dados..."
   sleep 2
 done
